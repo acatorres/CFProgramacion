fdf6cd9eefb2f940071bba2d280b91b9d1213e39
acatorres <acatorres@poligran.edu.co>
2026-09-08 10:59:13 -0500

    Translate user prompts to English in TileCalculator

--- src/TileCalculator.java
+++ src/TileCalculator.java
@@ -5,18 +5,18 @@ public class TileCalculator {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Ingrese la longitud de cada baldosa X (en metros): ");
+            System.out.print("Enter tile side length X (in meters): ");
             int x = scanner.nextInt();
-            System.out.print("Ingrese la longitud del piso N (en metros): ");
+            System.out.print("Enter floor length N (in meters): ");
             int n = scanner.nextInt();
-            System.out.print("Ingrese el ancho del piso M (en metros): ");
+            System.out.print("Enter floor width M (in meters): ");
             int m = scanner.nextInt();
             
             int tilesLength = (n + x - 1) / x;
             int tilesWidth = (m + x - 1) / x;
             
             int totalTiles = tilesLength * tilesWidth;
-            System.out.println("La cantidad de baldosas requeridas es: " + totalTiles);
+            System.out.println("Minimum number of tiles required: " + totalTiles);
         }
     }
 }
